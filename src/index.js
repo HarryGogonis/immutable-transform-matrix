2be9bfbc5c4926c81d93b5f9764994b9402130e6
Harry Gogonis <hgogonis@gmail.com>
2017-05-09 22:09:29 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import { Map } from 'extendable-immutable';
+/**
+Notes:
+- Is this a sparce matrix?
+*/
+
+export default class Matrix extends Map {
+  constructor(a, b, c, d, e, f) {
+    if (arguments.length > 0) {
+      super({ a, b, c, d, e, f });
+    } else {
+      super({
+        a: 1,
+        b: 0,
+        c: 0,
+        d: 1,
+        e: 0,
+        f: 0,
+      });
+    }
+  }
+
+  /**
+	 * Multiplies current matrix with new matrix values.
+	 * @param {number} a2 - scale x
+	 * @param {number} b2 - shear y
+	 * @param {number} c2 - shear x
+	 * @param {number} d2 - scale y
+	 * @param {number} e2 - translate x
+	 * @param {number} f2 - translate y
+	 */
+  transform(a2, b2, c2, d2, e2, f2) {
+    const a1 = this.get('a');
+    const b1 = this.get('b');
+    const c1 = this.get('c');
+    const d1 = this.get('d');
+    const e1 = this.get('e');
+    const f1 = this.get('f');
+
+    return this.withMutations(matrix => matrix
+      .set('a', (a1 * a2) + (c1 * b2))
+      .set('b', (b1 * a2) + (d1 * b2))
+      .set('c', a1 * c2 + c1 * d2)
+      .set('d', b1 * c2 + d1 * d2)
+      .set('e', a1 * e2 + c1 * f2 + e1)
+      .set('f', b1 * e2 + d1 * f2 + f1)
+    );
+  }
+
+  /**
+	 * Scales current matrix accumulative.
+   * If the second param is ommitted it scale uniformly.
+	 * @param {number} sx - scale factor x (1 does nothing)
+	 * @param {number} [sy] - scale factor y (1 does nothing)
+	 */
+  scale(...args) {
+    const sx = args[0];
+    let sy = sx;
+    if (args.length > 1) {
+      sy = args[1];
+    }
+    return this.transform(sx, 0, 0, sy, 0, 0);
+  }
+
+
+}
---
+++ tests/index.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai';
+import Matrix from '../src/index.js';
+const { describe, it } = global;
+
+describe('constructor', () => {
+  it('should create an identity Matrix', () => {
+    const matrix = new Matrix();
+    expect(matrix.get('a')).to.be.equal(1);
+    expect(matrix.get('b')).to.be.equal(0);
+    expect(matrix.get('c')).to.be.equal(0);
+    expect(matrix.get('d')).to.be.equal(1);
+    expect(matrix.get('e')).to.be.equal(0);
+    expect(matrix.get('f')).to.be.equal(0);
+  });
+
+  it('should create the specified Matrix', () => {
+    const [a, b, c, d, e, f] = [1, 2, 3, 4, 5, 6];
+    const matrix = new Matrix(a, b, c, d, e, f);
+    expect(matrix.get('a')).to.be.equal(a);
+    expect(matrix.get('b')).to.be.equal(b);
+    expect(matrix.get('c')).to.be.equal(c);
+    expect(matrix.get('d')).to.be.equal(d);
+    expect(matrix.get('e')).to.be.equal(e);
+    expect(matrix.get('f')).to.be.equal(f);
+  });
+});
+
+describe('scale', () => {
+  it('should create scale the Matrix', () => {
+    const matrix = new Matrix(1, 1, 1, 1, 1, 1).scale(2, 3);
+    expect(matrix.get('a')).to.be.equal(2);
+    expect(matrix.get('b')).to.be.equal(2);
+    expect(matrix.get('c')).to.be.equal(3);
+    expect(matrix.get('d')).to.be.equal(3);
+    expect(matrix.get('e')).to.be.equal(1);
+    expect(matrix.get('f')).to.be.equal(1);
+  });
+});
+
+describe('scale', () => {
+  it('should create scale the Matrix', () => {
+    const matrix = new Matrix(1, 1, 1, 1, 1, 1).scale(2);
+    expect(matrix.get('a')).to.be.equal(2);
+    expect(matrix.get('b')).to.be.equal(2);
+    expect(matrix.get('c')).to.be.equal(2);
+    expect(matrix.get('d')).to.be.equal(2);
+    expect(matrix.get('e')).to.be.equal(1);
+    expect(matrix.get('f')).to.be.equal(1);
+  });
+});
